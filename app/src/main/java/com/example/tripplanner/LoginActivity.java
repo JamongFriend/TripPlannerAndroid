fbeee697598da3b592b4e150b8a07d8589b53b85
JamogFrinds <ghrb6769@gmail.com>
2024-11-14 21:52:35 +0900

    feat: 로그인 화면 구현

---
+++ app/src/main/java/com/example/tripplanner/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.tripplanner;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}

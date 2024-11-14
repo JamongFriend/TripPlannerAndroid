555c1c0721d32e300df00e5cfef7106e4dfbd965
JamogFrinds <ghrb6769@gmail.com>
2024-11-14 21:51:54 +0900

    feat: 스플래시 화면 구현

--- app/src/main/java/com/example/tripplanner/MainActivity.java
+++ app/src/main/java/com/example/tripplanner/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.tripplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int SPLASH_TIMEOUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
     }
 }

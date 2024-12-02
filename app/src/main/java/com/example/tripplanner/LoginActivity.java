a6045bf1ba4fae6af6cf52ad651f7a3580ef2abf
Leehogyu <59430391+JamongFriend@users.noreply.github.com>
2024-12-02 17:51:08 +0900

    feat: 로그인 기능 구현

--- app/src/main/java/com/example/tripplanner/LoginActivity.java
+++ app/src/main/java/com/example/tripplanner/LoginActivity.java
@@ -1,13 +1,50 @@
 package com.example.tripplanner;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText emailEditText, passwordEditText;
+    private Button loginButton, forgotButton, registerButton;
+    ActivityResultLauncher<Intent> launcher;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+        forgotButton = findViewById(R.id.forgotButton);
+        registerButton = findViewById(R.id.registerButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailEditText.getText().toString();
+                String pw = passwordEditText.getText().toString();
+
+                Intent intent = new Intent(LoginActivity.this, Main.class);
+                intent.putExtra("ID", email);
+                intent.putExtra("PW", pw);
+                launcher.launch(intent);
+            }
+        });
+        launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+            if(result.getResultCode() == LoginActivity.RESULT_OK){
+                Intent data = result.getData();
+            }
+                } );
     }
 }

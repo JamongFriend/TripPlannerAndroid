601c0805709ca1de81c48f51879c40dc6a55f042
Leehogyu <59430391+JamongFriend@users.noreply.github.com>
2024-12-06 16:54:21 +0900

    fix: 스플래시 화면 수정

--- app/src/main/java/com/example/tripplanner/Splash.java
+++ app/src/main/java/com/example/tripplanner/Splash.java
@@ -6,6 +6,8 @@ import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.tripplanner.account.Login;
+
 public class Splash extends AppCompatActivity {
     private static final int SPLASH_TIMEOUT = 2000;
 
@@ -17,7 +19,7 @@ public class Splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash.this, LoginActivity.class);
+                Intent intent = new Intent(Splash.this, Login.class);
                 startActivity(intent);
                 finish();
             }
